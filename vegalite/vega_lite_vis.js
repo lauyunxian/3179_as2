1067a3cfddcbad1bc4511ccea6a8a1c4961ee071
ylau0019 <ylau0019@student.monash.edu>
2024-10-10 04:21:28 +0800

    add files

---
+++ vegalite/vega_lite_vis.js
@@ -0,0 +1,26 @@
+var vg_1 = "countries_inc_lvl_map.vg.json";
+
+vegaEmbed("#map", vg_1).then(function(result) {
+}).catch(console.error);
+
+var vg_2 = "avg_income.vg.json";
+
+vegaEmbed("#line", vg_2).then(function(result) {
+}).catch(console.error);
+
+var vg_3= "hdi_gini.vg.json";
+
+vegaEmbed("#bubble", vg_3).then(function(result) {
+}).catch(console.error);
+
+var vg_4= "waffle_chart.vg.json";
+
+vegaEmbed("#waffle", vg_4).then(function(result) {
+}).catch(console.error);
+
+var vg_5= "msia.vg.json";
+
+vegaEmbed("#msia", vg_5).then(function(result) {
+}).catch(console.error);
+
+
