494ec18126d8696d798b64866e38b5c49ef0c29d
ylau0019 <ylau0019@student.monash.edu>
2024-10-10 04:25:00 +0800

    changes

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,24 +1,24 @@
-var vg_1 = "countries_inc_lvl_map.vg.json";
+var vg_1 = "vegalite/countries_inc_lvl_map.vg.json";
 
 vegaEmbed("#map", vg_1).then(function(result) {
 }).catch(console.error);
 
-var vg_2 = "avg_income.vg.json";
+var vg_2 = "vegalite/avg_income.vg.json";
 
 vegaEmbed("#line", vg_2).then(function(result) {
 }).catch(console.error);
 
-var vg_3= "hdi_gini.vg.json";
+var vg_3= "vegalite/hdi_gini.vg.json";
 
 vegaEmbed("#bubble", vg_3).then(function(result) {
 }).catch(console.error);
 
-var vg_4= "waffle_chart.vg.json";
+var vg_4= "vegalite/waffle_chart.vg.json";
 
 vegaEmbed("#waffle", vg_4).then(function(result) {
 }).catch(console.error);
 
-var vg_5= "msia.vg.json";
+var vg_5= "vegalite/msia.vg.json";
 
 vegaEmbed("#msia", vg_5).then(function(result) {
 }).catch(console.error);
